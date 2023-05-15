b1053467c19068137cee8e601bc1e8cae5aa3d9a
fanerlight <972079854@qq.com>
2023-05-15 10:56:42 +0800

    search param

--- src/batch_process.py
+++ src/batch_process.py
@@ -3,13 +3,16 @@ import json
 
 json_file='./params/pavia_contra_mask.json'
 
-dims=[32]
+# labels=[0.5,1.0]
+# temps=[15,20,25,30,35,40,45,50]
 
-# for dim in dims:
-#     with open(json_file,'r') as fin:
-#         config_in=json.load(fin)
-#     config_in['net']['dim']=dim
-#     with open(json_file,'w') as fout:
-#         json.dump(config_in,fout,indent=4)
+# for l in labels:
+#     for t in temps:
+#         with open(json_file,'r') as fin:
+#             config_in=json.load(fin)
+#         config_in['data']['unlabelled_multiple']=l
+#         config_in['train']['temp']=t
+#         with open(json_file,'w') as fout:
+#             json.dump(config_in,fout,indent=4)
 for i in range(10):
     os.system("python ./workflow.py")
