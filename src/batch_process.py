b32db3a23e6b2967f3ea2bc3d368e0ad8e073a61
fanerlight <972079854@qq.com>
2023-05-17 17:55:23 +0800

    tuning

--- src/batch_process.py
+++ src/batch_process.py
@@ -3,13 +3,18 @@ import json
 
 json_file='./params/pavia_contra_mask.json'
 
-dims=[56,48,36]
+patches=[13,15,17]
 
-for dim in dims:
+for patch in patches:
     with open(json_file,'r') as fin:
         config_in=json.load(fin)
-    config_in['net']['dim']=dim
+    config_in['data']['patch_size']=patch
     with open(json_file,'w') as fout:
         json.dump(config_in,fout,indent=4)
     for i in range(10):
         os.system("python ./workflow.py")
+
+
+
+# for i in range(10):
+#     os.system("python ./workflow.py")
