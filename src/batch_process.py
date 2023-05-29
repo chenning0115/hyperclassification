d3d8db3dd446b0fb330e6421355b1a80433eecc9
chenning <chenning0115@gmail.com>
2023-05-29 15:55:10 +0800

    diffusion fix

--- src/batch_process.py
+++ src/batch_process.py
@@ -6,10 +6,10 @@ from workflow import train_by_param, train_convention_by_param
 exchange_json_file='%s/temp.json' % config_path_prefix
 
 def simple_run_times():
-    # times = 5 #每个配置跑5次
-    # sample_num = [10, 20, 30, 40, 50, 60, 70, 80]
-    times = 1 #每个配置跑5次
-    sample_num = [40]
+    times = 5 #每个配置跑5次
+    sample_num = [10, 20, 30, 40, 50, 60, 70, 80]
+    # times = 1 #每个配置跑5次
+    # sample_num = [40]
 
     configs = [
         'indian_cross_param_use.json',
@@ -22,12 +22,12 @@ def simple_run_times():
             params = json.loads(fin.read())
             data_sign = params['data']['data_sign']
             for num in sample_num:
-                for t in times:
-                    uniq_name = "%s_%s_%s" % (data_sign, num, t)
+                for t in range(times):
+                    uniq_name = "%s_%s_%s" % (config_name, num, t)
                     params['data']['data_file'] = '%s_%s' % (data_sign, num)
                     params['uniq_name'] = uniq_name
                     with open(exchange_json_file,'w') as fout:
-                        json.dump(exchange_json_file,fout)
+                        json.dump(params,fout)
                     print("schedule %s..." % uniq_name)
                     os.system('python ./workflow.py')
                     print("schedule done of %s..." % uniq_name)
